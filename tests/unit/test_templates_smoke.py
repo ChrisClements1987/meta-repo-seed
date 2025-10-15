92ebd079c421371fbb4c7d3ee450cafd55a333a8
Chris Clements <clements.christopher1@gmail.com>
2025-10-15 11:06:16 +0100

    test(unit): correct import to ProductTemplateRegistry in templates smoke test

--- tests/unit/test_templates_smoke.py
+++ tests/unit/test_templates_smoke.py
@@ -1,11 +1,11 @@
 import pytest
 
-from src.templates.generator import TemplateRegistry
+from src.templates.generator import ProductTemplateRegistry
 
 
 @pytest.mark.unit
 def test_list_templates_non_empty():
-    names = TemplateRegistry.list_templates()
+    names = ProductTemplateRegistry.list_templates()
     assert isinstance(names, list)
     assert len(names) > 0
 
